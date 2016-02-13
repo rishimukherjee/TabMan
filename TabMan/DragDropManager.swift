09e69f08dc1053350b3a4cc3e788e0791096b602
Rishi Mukherjee <rishi@gopassport.com>
2016-02-13 15:51:26 +0530

    - Tables cannot be dropped on any other table - Tables cannot be dropped out side the floor.

--- TabMan/DragDropManager.swift
+++ TabMan/DragDropManager.swift
@@ -32,6 +32,7 @@ class DragDropManager: NSObject {
 
     func removeTableFromFloor(table: TableImageView) {
         if let context = self.dragContext {
+            // Only tables from floor can be removed
             if context.draggedFromType == .Floor {
                 if let tableAtFloorIndex = tables.indexOf(table) {
                     table.removeFromSuperview()
@@ -48,6 +49,48 @@ class DragDropManager: NSObject {
         }
     }
 
+    func isTableOnFloor(table: TableImageView, position: CGPoint) -> Bool {
+        let viewBeingDraggedWidth = CGRectGetWidth(table.frame)
+        let viewBeingDraggedHeight = CGRectGetHeight(table.frame)
+
+        var leftCornerInDropView = position
+        leftCornerInDropView.x -= viewBeingDraggedWidth / 2.0
+
+        var rightCornerInDropView = position
+        rightCornerInDropView.x += viewBeingDraggedWidth / 2.0
+
+        var topCornerInDropView = position
+        topCornerInDropView.y -= viewBeingDraggedHeight / 2.0
+
+        var bottomCornerInDropView = position
+        bottomCornerInDropView.y += viewBeingDraggedHeight / 2.0
+
+        if floor.pointInside(leftCornerInDropView, withEvent: nil)
+            && floor.pointInside(rightCornerInDropView, withEvent: nil)
+            && floor.pointInside(topCornerInDropView, withEvent: nil)
+            && floor.pointInside(bottomCornerInDropView, withEvent: nil)
+        {
+            return true
+        }
+        return false
+    }
+
+    func isTablePositionValidOnFloor(table: TableImageView) -> Bool {
+
+        for otherTable in tables {
+            if otherTable != table {
+                // Convert to table's coordinate
+                let rectInMainView = table.superview!.convertRect(otherTable.frame, fromView: otherTable.superview)
+                if CGRectIntersectsRect(table.frame, rectInMainView) {
+                    print("Intersection happened!")
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
     func dragging(sender: UIPanGestureRecognizer) {
         print("Dragging..")
         switch sender.state {
@@ -55,7 +98,6 @@ class DragDropManager: NSObject {
             for table in tables + pickers {
                 let pointInSubjectsView = sender.locationInView(table)
                 let pointInsideDraggableObject = table.pointInside(pointInSubjectsView, withEvent: nil)
-
                 let tableOn: TableOn
 
                 if table.superview! == floor {
@@ -74,7 +116,7 @@ class DragDropManager: NSObject {
                         sender.view?.addSubview(table)
                         self.dragTableWithGesture(sender)
                     case .PickerContainer:
-                        print("Dragging from picket container!")
+                        print("Dragging from picker container!")
                         // Create a new table which can be kept on the floor and drag that instead of dragging the picker
                         guard let draggedTableType = table.type else {
                             return
@@ -116,25 +158,34 @@ class DragDropManager: NSObject {
             if let context = self.dragContext {
                 let viewBeingDragged = context.draggedView
                 var droppedViewInFloor = false
-
                 let pointInDropView = sender.locationInView(floor)
+                var viewInFloorButIntersectsWithOtherTable = false
 
-                if floor.pointInside(pointInDropView, withEvent: nil) {
-                    droppedViewInFloor = true
-                    viewBeingDragged.removeFromSuperview()
-                    floor.addSubview(viewBeingDragged)
-                    viewBeingDragged.frame = CGRect(
-                        x: pointInDropView.x - (CGRectGetWidth(viewBeingDragged.frame) / 2),
-                        y: pointInDropView.y - (CGRectGetHeight(viewBeingDragged.frame) / 2),
-                        width: CGRectGetWidth(viewBeingDragged.frame),
-                        height: CGRectGetHeight(viewBeingDragged.frame)
-                    )
-                    self.addTableToFloor(viewBeingDragged)
+                if isTableOnFloor(viewBeingDragged, position: pointInDropView) {
+                    if isTablePositionValidOnFloor(viewBeingDragged) {
+                        droppedViewInFloor = true
+                        viewBeingDragged.removeFromSuperview()
+                        floor.addSubview(viewBeingDragged)
+                        viewBeingDragged.frame = CGRect(
+                            x: pointInDropView.x - (CGRectGetWidth(viewBeingDragged.frame) / 2),
+                            y: pointInDropView.y - (CGRectGetHeight(viewBeingDragged.frame) / 2),
+                            width: CGRectGetWidth(viewBeingDragged.frame),
+                            height: CGRectGetHeight(viewBeingDragged.frame)
+                        )
+                        self.addTableToFloor(viewBeingDragged)
+                    } else {
+                        viewInFloorButIntersectsWithOtherTable = true
+                    }
+
                 }
                 if !droppedViewInFloor {
                     // If moved from floor to pickerContainer, delete that table from floor
                     if context.draggedFromType == .Floor {
-                        self.removeTableFromFloor(context.draggedView)
+                        if viewInFloorButIntersectsWithOtherTable {
+                            context.moveToOriginalPosition()
+                        } else {
+                            self.removeTableFromFloor(context.draggedView)
+                        }
                     } else {
                         context.moveToOriginalPosition()
                     }
